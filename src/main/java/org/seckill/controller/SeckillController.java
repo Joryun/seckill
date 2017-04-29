14c90aa65c7c794d1c564629e164b692f510f873
Joryun <m18814128406@163.com>
2017-04-29 20:43:14 +0800

    实现秒杀相关的Restful接口，编写SeckillController类，封装一个所有ajax请求返回类型的DTO类SeckillResult

---
+++ src/main/java/org/seckill/controller/SeckillController.java
@@ -0,0 +1,78 @@
+package org.seckill.controller;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillResult;
+import org.seckill.entity.Seckill;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by joryun on 2017/4/29.
+ */
+
+//@Controller 将Controller放入Spring容器之中
+@Controller
+@RequestMapping("/seckill")     //url:/模块/资源/{id}/细分 eg: /seckill/list
+public class SeckillController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+
+        //获取列表页
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+
+        //list.jsp + model = ModelAndView
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+
+        if (seckillId == null) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if (seckill == null) {
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+
+        return "detail";
+    }
+
+    //ajax json
+    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId) {
+
+        SeckillResult<Exposer> result;
+
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/seckill/dto/SeckillResult.java
@@ -0,0 +1,49 @@
+package org.seckill.dto;
+
+/**
+ * Created by joryun on 2017/4/29.
+ */
+
+//所有ajax请求返回类型，封装json结果
+public class SeckillResult<T> {
+
+    private boolean success;
+
+    private T data;
+
+    private String error;
+
+    public SeckillResult(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public SeckillResult(boolean success, String error) {
+        this.success = success;
+        this.error = error;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
